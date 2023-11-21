0b4de375ef93fd3eb24fd5513e4b3a7f7fdc6cdb
Lucas Avelar <ldavela@clemson.edu>
2023-11-21 08:10:15 -0500

    Full Draft Rmd File complete

--- code/word-vector-analysis.R
+++ code/word-vector-analysis.R
@@ -13,68 +13,43 @@ library(magrittr)
 
 if (!file.exists("word_embbedings/fullcorpus_bngrams.txt")) prep_word2vec(origin="txt_files/",destination="word_embbedings/fullcorpus_bngrams.txt",lowercase=T,bundle_ngrams=2)
 
-if (!file.exists("word_embbedings/fullcorpus_bngrams.bin")) {model = train_word2vec("word_embbedings/fullcorpus_bngrams.txt","word_embbedings/fullcorpus_bngrams.bin",vectors=200,threads=4,window=30,iter=30,negative_samples=0)} else model = read.vectors("word_embbedings/fullcorpus_bngrams.bin")
+if (!file.exists("word_embbedings/fullcorpus_bngrams.bin")) {model = train_word2vec("word_embbedings/fullcorpus_bngrams.txt","word_embbedings/fullcorpus_bngrams.bin",vectors=50,threads=4,window=30,iter=30,negative_samples=0)} else model = read.vectors("word_embbedings/fullcorpus_bngrams.bin")
 
-model %>% closest_to("indian")
+model <- model[-1,] #remove "</s>" (first row) from the model as it seems to be an OCR error
 
-model %>% 
-  closest_to(model[[c("philippine","philippines","filipino","igorrote","manila","igorrot","filipinos")]],50)
+model %>% closest_to("savage", 20)
 
-model %>% 
-  closest_to(model[[c("indian","native","tribe","muskogee","sioux","cocopah","indians")]],50)
+model %>% closest_to("progress", 20)
 
+#visualize semantic similarity of "savage" and "progress" based on the preliminary lists above
+modernity = closest_to(model,model[[c("progress", "civilization", "science")]], 60)
+vectors_modernity = model[[modernity$word,average=F]]
+plot(vectors_modernity,method="pca")
 
-#visualize indian model
-indian = closest_to(model,model[[c("indian","native","tribe","muskogee","sioux","cocopah","indians")]],150)
-average_indian = model[[indian$word,average=F]]
-plot(average_indian,method="pca")
+otherness = closest_to(model,model[[c("savage", "primitive", "barbaric")]], 60)
+vectors_otherness = model[[otherness$word,average=F]]
+plot(vectors_otherness,method="pca")
 
-#visualize philippine model
-philippines = closest_to(model,model[[c("philippine","philippines","filipino","igorrote","manila","igorrot","filipinos")]],150)
-average_philippines = model[[philippines$word,average=F]]
-plot(average_philippines,method="pca")
+##plot semantic similarity of "savage" and "progress" together
+otherness_modernity = closest_to(model, model[[c("savage", "primitive", "barbaric", "progress", "civilization", "science")]],100)
+vectors_otherness_modernity = model[[otherness_modernity$word,average=F]]
+plot(vectors_otherness_modernity,method="pca")
 
-
-## trying again without bundling ngrams
-
-#build up / train the model
-if (!file.exists("word_embbedings/fullcorpus.txt")) prep_word2vec(origin="txt_files/",destination="word_embbedings/fullcorpus.txt",lowercase=T)
-
-if (!file.exists("word_embbedings/fullcorpus.bin")) {model = train_word2vec("word_embbedings/fullcorpus.txt","word_embbedings/fullcorpus.bin",vectors=200,threads=4,window=30,iter=30,negative_samples=0)} else model = read.vectors("word_embbedings/fullcorpus.bin")
-
-model %>% closest_to("indian")
-
-model %>% 
-  closest_to(model[[c("philippine","philippines","filipino","igorrote","manila","igorrot","filipinos")]],50)
-
-model %>% 
-  closest_to(model[[c("indian","native","tribe","muskogee","sioux","cocopah","indians")]],50)
-
-
-#visualize indian model
-indian = closest_to(model,model[[c("indian","native","tribe","muskogee","sioux","cocopah","indians")]],150)
-average_indian = model[[indian$word,average=F]]
-plot(average_indian,method="pca")
-
-#visualize philippine model
-philippines = closest_to(model,model[[c("philippine","philippines","filipino","igorrote","manila","igorrot","filipinos")]],150)
-average_philippines = model[[philippines$word,average=F]]
-plot(average_philippines,method="pca")
 
 #clustering across the full corpus (like topic modeling but less sophisticated)
-set.seed(10)
+set.seed(15)
 centers = 150
-clustering = kmeans(model,centers=centers,iter.max = 40)
+clustering = kmeans(model,centers=centers,iter.max = 200)
 
 sapply(sample(1:centers,10),function(n) {
   names(clustering$cluster[clustering$cluster==n][1:10])
 })
 
-#clustering based on 5 main topics
-topics = c("philippine","indian","sioux","cocopah", "igorrote")
+#clustering based on 2 main topics
+topics = c("savage", "progress")
 term_set = lapply(topics, 
                   function(topic) {
-                    nearest_words = model %>% closest_to(model[[topic]],20)
+                    nearest_words = model %>% closest_to(model[[topic]],15)
                     nearest_words$word
                   }) %>% unlist
 
